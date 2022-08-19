73e0614c357775d76cbf51baf45c779901a8f4a9
trofi92 <84131398+trofi92@users.noreply.github.com>
2022-08-19 22:22:49 +0900

    fuck

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React,{useState, useEffect} from "react";
+import AppRouter from "./Router";
+import {authService} from "../fbase";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (<>
+    <AppRouter isLoggedIn={isLoggedIn} />
+    <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, HashRouter as Router, Route } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = ({ isLoggedIn }) => {
+    return (
+    <Router>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home/>}/>
+          </>
+                ) : (
+            <>
+          <Route path="/" element={<Auth/>}/>
+          </>
+        )}
+      </Routes>
+    </Router>
+  );
+};
+export default AppRouter;
